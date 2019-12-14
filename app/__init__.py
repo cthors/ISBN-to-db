46d00642e96d48466b0ed540f6d665227cfe7734
Casey <thorsenkm@gmail.com>
2019-12-14 02:23:59 +0000

    added config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 from werkzeug.debug import DebuggedApplication
 
 app = Flask(__name__)
 app.debug = True
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+app.config.from_object(Config)
 
 from app import routes
