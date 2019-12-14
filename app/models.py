b9bcddedc7bda6a376ef5a1190113ba89130f598
Casey <thorsenkm@gmail.com>
2019-12-14 21:04:01 +0000

    deleted migration

--- app/models.py
+++ app/models.py
@@ -3,6 +3,14 @@ from app import db
 class Book(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	json = db.Column(db.String(7000), unique=False, nullable=True)
+	author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
 
 	def __repr__(self):
-		return format(json)
+		return format(self.json)
+
+class Author(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	json = db.Column(db.String(7000), unique=False, nullable=True)
+
+	def __repr__(self):
+		return format(self.json)
