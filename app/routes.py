4888e4eef5a98f7577e3246c7767d74f1563d544
Casey <thorsenkm@gmail.com>
2019-12-17 00:24:37 +0000

    committing before updating model and db

--- app/routes.py
+++ app/routes.py
@@ -3,15 +3,19 @@ from app.showRecords import ShowRecs
 from app.addRecords import AddRecs
 from flask import render_template
 
+@app.route('/')
+def homepage():
+	return render_template('index.html')
+
 @app.route('/list')
 def show_recs():
 	displayItems = ShowRecs.testShow()
-	# just for debugging
-	return render_template('index.html', newBooks=displayItems, oldBooks=displayItems)
+	# just for debugging now
+	return render_template('list.html', newBooks=displayItems, oldBooks=displayItems)
 
-@app.route('/')
+@app.route('/add')
 def add_recs():
 	booksToList = AddRecs.testAdd()
 	addedBooks = booksToList[0]
 	existingBooks = booksToList[1]
-	return render_template('index.html', newBooks=addedBooks, oldBooks=existingBooks)
+	return render_template('list.html', newBooks=addedBooks, oldBooks=existingBooks)
