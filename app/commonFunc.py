0323fcb493e12750fa1a138eb3d5cde0da1f7dfd
unknown <kmthorsen@gmail.com>
2020-03-14 15:32:39 -0300

    onebook page

--- app/commonFunc.py
+++ app/commonFunc.py
@@ -25,4 +25,5 @@ class CommonFunctions():
 
 	def oneBookInfo(bookId):
 		str = Book.query.filter_by(_bookId=bookId).first()._bookJson
-		return str
+		pp = pprint.PrettyPrinter(width=41, compact=True)
+		return pp.pprint(str)
