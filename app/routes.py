be3e9a932a4603895ded0455de4c9f545e123fd1
unknown <kmthorsen@gmail.com>
2020-03-14 19:46:11 -0300

    onebook page

--- app/routes.py
+++ app/routes.py
@@ -21,9 +21,9 @@ def add_records():
 							noBooks=results[2],
 							debugList=results[3])
 
-@app.route('/set_lastname/<lname>')
-def set_lastname(lname):
-	return "set last name for sort"
+@app.route('/set_lastname/<lname>/<bookId>')
+def set_lastname(lname, id):
+	return "set last name for sort: " + lname + " for " + bookId
 
 @app.route('/<id>')
 def one_book_info(id):
