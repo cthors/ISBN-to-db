39c652bf5dc7bae819edea0db92a16e87a12d086
unknown <kmthorsen@gmail.com>
2020-03-14 21:46:59 -0300

    onebook page

--- app/addRecords.py
+++ app/addRecords.py
@@ -98,9 +98,7 @@ def putBookInDb(bookKey, isbn):
 	if 'subtitle' in bookJson:
 		b._subtitle = bookJson['subtitle']
 	if 'publish_date' in bookJson:
-		splitDate = bookJson['publish_date'].split()
-		b._date = bookJson['publish_date']
-	#	b._date = splitDate[2] + splitDate[0] + splitDate[1][1:-1]
+		b._date = bookJson['publish_date'].split()
 	db.session.add(b)
 	db.session.commit() # commit to get the ID
 	pb = PhysicalBook(_bookId=b._id)
